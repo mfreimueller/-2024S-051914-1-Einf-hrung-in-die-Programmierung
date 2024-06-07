54c280532fdeec8a93e1e190eae91826a1cff72b
Michael Freimüller <michael@mfreimueller.com>
2024-06-07 09:46:50 +0200

    add try/catch and a flag to work around uncertainties regarding the task at hand

--- Uebung 6/Freimueller_Uebung_6.py
+++ Uebung 6/Freimueller_Uebung_6.py
@@ -1,15 +1,28 @@
-in_file = open("input_data.txt", "r")
+# Nachdem ich nicht zu 100% sicher bin, welche Anweisung
+# am Aufgabenblatt korrekt ist, habe ich hier eine Kompromisslösung:
+# Wenn der fettgedruckte Text, dass nur die geraden Zahlen multipliziert
+# werden sollen, wahr ist, so setzt man multiply_even_numbers auf True.
+# Wenn aber die Anweisung unter Schritt 2 gültig ist, so setzt man 
+# multiply_even_numbers auf False.
+multiply_even_numbers = True
+
 input_number_list = []
 
-line = in_file.readline()
-
-while line != "":
-    if line.strip().isdecimal():
-        input_number_list.append(int(line))
-
+try: # safety first.
+    in_file = open("input_data.txt", "r")
+    
     line = in_file.readline()
 
-in_file.close()
+    while line != "":
+        if line.strip().isdecimal():
+            input_number_list.append(int(line))
+
+        line = in_file.readline()
+
+    in_file.close()
+except FileNotFoundError:
+    print("Die Datei 'input_data.txt' wurde nicht gefunden!")
+    exit()
 
 def process_number_list(number_list):
     product = 1
@@ -17,7 +30,7 @@ def process_number_list(number_list):
     for number in number_list:
         # Wenn man das Produkt der ungeraden Zahlen berechnen wollte,
         # müsste man aus der unteren Zeile ein number % 2 == 1 machen.
-        if number % 2 == 0:
+        if number % 2 == (0 if multiply_even_numbers else 1):
             product *= number
     
     return product
